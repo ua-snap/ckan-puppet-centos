e06e945756d1bb89a43264d4f7442599652463ca
Bob Torgerson <rltorgerson@alaska.edu>
2015-01-12 17:49:12 -0900

    Installs PostgreSQL database with default user.

--- puppet/modules/ckan/manifests/install.pp
+++ puppet/modules/ckan/manifests/install.pp
@@ -21,11 +21,12 @@ class ckan::install {
 
   # Install CKAN deps
   $ckan_libs = ['libcurl-devel','httpd', 'xml-common', 'git', 'libxslt', 'libxslt-devel',
-                'libxml2', 'libxml2-devel', 'gcc', 'gcc-c++', 'make','postgresql','postgresql-devel',
+                'libxml2', 'libxml2-devel', 'gcc', 'gcc-c++', 'make',
                 'java-1.6.0-openjdk-devel', 'java-1.6.0-openjdk', 'tomcat6', 'xalan-j2', 'unzip',
                 'policycoreutils-python','mod_wsgi','xml-commons-resolver','xml-commons-apis']
   package { $ckan_libs: ensure => present, }
 
+# postgresql-devel
 
   class { 'python':
     version    => 'system',
